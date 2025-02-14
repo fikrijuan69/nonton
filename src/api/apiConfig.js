5196657b0a43d87512838a38607997c0bfba9147
fikrijuan69 <juandafikri8@gmail.com>
2025-02-14 22:28:40 +0700

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "e3fa8bf7e067cea74ae03c3575fe7562",
+  apiKey: "",
   originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
